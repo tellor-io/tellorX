87461fc1b8b92a2477f28559fa0201bb94c23e95
themandalore <fett.nicholas@yahoo.com>
2021-07-05 10:08:15 -0400

    Update TestGovernance.sol

--- contracts/testing/TestGovernance.sol
+++ contracts/testing/TestGovernance.sol
@@ -2,7 +2,13 @@
 pragma solidity 0.8.3;
 
 import "../Governance.sol";
-
 contract TestGovernance is Governance{
-
+    event Received(address, uint);
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+    //allows the contract to recieve funds for gas via harhdat-impersonate account
+    fallback() external payable{
+        emit Received(msg.sender, msg.value);
+    };
 }
