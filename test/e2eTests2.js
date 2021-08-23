41ca01933065a5af099150f4dd6a870e8dc17229
Tally Wiesenberg <t.j.wies@gmail.com>
2021-08-23 13:31:07 -0400

    added coverage

--- test/e2eTests2.js
+++ test/e2eTests2.js
@@ -344,7 +344,7 @@ describe("End-to-End Tests - Two", function() {
 
     })
 
-    it("stake enough reporters to prove disputeFee hits minimum", async function() {
-      
+    it("Test dispute on old contract, then dispute on new contract", async function() {
+
     })
 });
