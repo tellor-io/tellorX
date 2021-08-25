e39bba36f81ce56eabd6912d608cef1d85be8946
cpondoc <chris.pondoc@comcast.net>
2021-08-25 11:19:19 -0400

    Documentation for Staking contract

--- contracts/TellorStaking.sol
+++ contracts/TellorStaking.sol
@@ -5,71 +5,99 @@ import "./interfaces/IGovernance.sol";
 import "hardhat/console.sol";
 
 contract TellorStaking is Token{
-
+    // Events
     event NewStaker(address _staker);
     event StakeWithdrawRequested(address _staker);
     event StakeWithdrawn(address _staker);
 
+    // Functions
+    /**
+     * @dev Changes staking status of a reporter
+     * Note: this function is only callable by the Governance contract.
+     * @param _reporter is the address of the reporter to change staking status for
+     * @param _status is the new status of the reporter
+    */
     function changeStakingStatus(address _reporter, uint _status) external{
         require(msg.sender == addresses[_GOVERNANCE_CONTRACT]);
         StakeInfo storage stakes = stakerDetails[_reporter];
         stakes.currentStatus = _status;
     }
 
+    /**
+     * @dev Allows a reporter to submit stake
+    */
     function depositStake() external{
+        // Ensure staker has enough balance to stake
         require(
             balances[msg.sender][balances[msg.sender].length - 1].value >=
             uints[_STAKE_AMOUNT],
             "Balance is lower than stake amount"
         );
-        //Ensure staker is not currently staked or is locked for withdraw
+        // Ensure staker is not currently staked or is locked for withdraw
         require(
             stakerDetails[msg.sender].currentStatus == 0 ||
             stakerDetails[msg.sender].currentStatus == 2,
-            "Miner is in the wrong state"
+            "Reporter is in the wrong state"
         );
+        // Increment number of stakers, create new staker, and update dispute fee
         uints[_STAKE_COUNT] += 1;
         stakerDetails[msg.sender] = StakeInfo({
             currentStatus: 1, 
-            startDate: block.timestamp//this resets their stake start date to now
+            startDate: block.timestamp // This resets their stake start date to now
         });
         emit NewStaker(msg.sender);
         IGovernance(addresses[_GOVERNANCE_CONTRACT]).updateMinDisputeFee();
     }
 
+    /**
+     * @dev Allows a reporter to request to withdraw their stake
+    */
     function requestStakingWithdraw() external {
+        // Ensures reporter is not already staked
         StakeInfo storage stakes = stakerDetails[msg.sender];
-        require(stakes.currentStatus == 1, "Miner is not staked");
+        require(stakes.currentStatus == 1, "Reporter is not staked");
+        // Change status to reflect withdraw request and updates start date for staking
         stakes.currentStatus = 2;
         stakes.startDate = block.timestamp;
+        // Update number of stakers and dispute fee
         uints[_STAKE_COUNT] -= 1;
         IGovernance(addresses[_GOVERNANCE_CONTRACT]).updateMinDisputeFee();
         emit StakeWithdrawRequested(msg.sender);
     }
 
+    /**
+     * @dev Slashes a reporter and transfers their stake amount to their disputer
+     * Note: this function is only callable by the Governance contract.
+     * @param _reporter is the address of the reporter being slashed
+     * @param _disputer is the address of the disputer receiving the reporter's stake
+    */
     function slashMiner(address _reporter, address _disputer) external{
-        //require that only the governance contract can call it
         require(msg.sender == addresses[_GOVERNANCE_CONTRACT]);
-        stakerDetails[_reporter].currentStatus = 5;
+        stakerDetails[_reporter].currentStatus = 5; // Change status of reporter to slashed
+        // Transfer stake amount of reporter has a balance bigger than the stake amount
         if(balanceOf(_reporter) >= uints[_STAKE_AMOUNT]){
             _doTransfer(_reporter,_disputer,uints[_STAKE_AMOUNT]);
         }
-        //in case we increase stake amount over their balance
+        // Else, transfer all of the reporter's balance
         else if(balanceOf(_reporter) > 0){
             _doTransfer(_reporter,_disputer,balanceOf(_reporter));
         }
     }
 
+    /**
+     * @dev Withdraws a reporter's stake
+    */
     function withdrawStake() external {
         StakeInfo storage _s = stakerDetails[msg.sender];
-        require(block.timestamp - _s.startDate >=  7 days,"7 days didn't pass");
+        // Ensure reporter is locked and that enough time has passed
+        require(block.timestamp - _s.startDate >=  7 days, "7 days didn't pass");
         require(_s.currentStatus == 2,"Reporter not locked for withdrawal");
-        _s.currentStatus = 0;
+        _s.currentStatus = 0; // Updates status to withdrawn
         emit StakeWithdrawn(msg.sender);
     }
     
     /**
-     * @dev This function allows users to retrieve all information about a staker
+     * @dev Allows users to retrieve all information about a staker
      * @param _staker address of staker inquiring about
      * @return uint current state of staker
      * @return uint startDate of staking
