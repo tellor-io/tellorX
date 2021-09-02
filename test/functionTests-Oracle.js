a9c3b50cdc4742eabb26c25c1af63bd9992d56a5
Tim <tpkernell@gmail.com>
2021-09-02 10:35:04 -0500

    update to use parachute as deity

--- test/functionTests-Oracle.js
+++ test/functionTests-Oracle.js
@@ -3,16 +3,20 @@ const { expect } = require("chai");
 const h = require("./helpers/helpers");
 var assert = require('assert');
 const web3 = require('web3');
-const { Console } = require("console");
+const { ethers } = require("hardhat");
+const { stakeAmount } = require("./helpers/helpers");
+const { keccak256 } = require("ethers/lib/utils");
 
-describe("TellorX Function Tests - Oracle", function() {
+describe("End-to-End Tests - Three", function() {
 
-    const tellorMaster = "0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0"
-    const DEV_WALLET = "0x39E419bA25196794B595B2a595Ea8E527ddC9856"
-    let accounts = null
-    let tellor = null
-    let cfac,ofac,tfac,gfac,devWallet
-    let govSigner = null
+  const tellorMaster = "0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0"
+  const DEV_WALLET = "0x39E419bA25196794B595B2a595Ea8E527ddC9856"
+  const PARACHUTE = "0x83eB2094072f6eD9F57d3F19f54820ee0BaE6084"
+  const BIGWALLET = "0xf977814e90da44bfa03b6295a0616a897441acec";
+  let accounts = null
+  let tellor = null
+  let cfac,ofac,tfac,gfac,parachute,govBig,govTeam
+  let govSigner = null
 
   beforeEach("deploy and setup TellorX", async function() {
     accounts = await ethers.getSigners();
@@ -20,23 +24,32 @@ describe("TellorX Function Tests - Oracle", function() {
       method: "hardhat_reset",
       params: [{forking: {
             jsonRpcUrl: hre.config.networks.hardhat.forking.url,
-            blockNumber:12762660
+            blockNumber:13037866
+
           },},],
       });
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
       params: [DEV_WALLET]}
     )
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [PARACHUTE]}
+    )
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [BIGWALLET]}
+    )
         //Steps to Deploy:
         //Deploy Governance, Oracle, Treasury, and Controller.
         //Fork mainnet Ethereum, changeTellorContract to Controller
         //run init in Controller
 
-    oldTellorInstance = await ethers.getContractAt("contracts/interfaces/ITellor.sol:ITellor", tellorMaster)
+    oldTellorInstance = await ethers.getContractAt("contracts/tellor3/ITellor.sol:ITellor", tellorMaster)
     gfac = await ethers.getContractFactory("contracts/testing/TestGovernance.sol:TestGovernance");
     ofac = await ethers.getContractFactory("contracts/Oracle.sol:Oracle");
     tfac = await ethers.getContractFactory("contracts/Treasury.sol:Treasury");
-    cfac = await ethers.getContractFactory("contracts/Controller.sol:Controller");
+    cfac = await ethers.getContractFactory("contracts/testing/TestController.sol:TestController");
     governance = await gfac.deploy();
     oracle = await ofac.deploy();
     treasury = await tfac.deploy();
@@ -47,9 +60,21 @@ describe("TellorX Function Tests - Oracle", function() {
     await controller.deployed();
     await accounts[0].sendTransaction({to:DEV_WALLET,value:ethers.utils.parseEther("1.0")});
     devWallet = await ethers.provider.getSigner(DEV_WALLET);
+    bigWallet = await ethers.provider.getSigner(BIGWALLET);
     master = await oldTellorInstance.connect(devWallet)
-    await master.changeTellorContract(controller.address);
+    await master.proposeFork(controller.address);
+    let _id = await master.getUintVar(h.hash("_DISPUTE_COUNT"))
+    await master.vote(_id,true)
+    master = await oldTellorInstance.connect(bigWallet)
+    await master.vote(_id,true);
+    await h.advanceTime(86400 * 8)
+    await master.tallyVotes(_id)
+    await h.advanceTime(86400 * 2.5)
+    await master.updateTellor(_id)
     tellor = await ethers.getContractAt("contracts/interfaces/ITellor.sol:ITellor",tellorMaster, devWallet);
+    parachute = await ethers.getContractAt("contracts/interfaces/ITellor.sol:ITellor",PARACHUTE, devWallet);
+    govTeam = await ethers.getContractAt("contracts/interfaces/ITellor.sol:ITellor",governance.address, devWallet);
+    govBig = await ethers.getContractAt("contracts/interfaces/ITellor.sol:ITellor",governance.address, bigWallet);
     await tellor.deployed();
     await tellor.init(governance.address,oracle.address,treasury.address)
     await hre.network.provider.request({
@@ -58,7 +83,7 @@ describe("TellorX Function Tests - Oracle", function() {
     )
     await accounts[1].sendTransaction({to:governance.address,value:ethers.utils.parseEther("1.0")});
     govSigner = await ethers.provider.getSigner(governance.address);
-  });
+    });
   it("addTip()", async function() {
     var ts = await tellor.totalSupply()
     oracle1 = await ethers.getContractAt("contracts/Oracle.sol:Oracle",oracle.address, accounts[1]);
@@ -75,7 +100,6 @@ describe("TellorX Function Tests - Oracle", function() {
   it("submitValue()", async function() {
     await tellor.transfer(accounts[1].address,web3.utils.toWei("100"));
     await tellor.transfer(accounts[2].address,web3.utils.toWei("200"));
-    await tellor.transfer(oracle.address,web3.utils.toWei("200"));//funding the oracle for inflationary rewards
     tellorUser = await ethers.getContractAt("contracts/interfaces/ITellor.sol:ITellor",tellorMaster, accounts[1]);
     let nonce = await oracle.getTimestampCountById(ethers.utils.formatBytes32String("1"));
     await h.expectThrow(oracle.submitValue( ethers.utils.formatBytes32String("1"),150,nonce));//must be staked
@@ -84,6 +108,7 @@ describe("TellorX Function Tests - Oracle", function() {
     await tellorUser.depositStake();
     oracle2 = await ethers.getContractAt("contracts/interfaces/ITellor.sol:ITellor",oracle.address, accounts[2]);
     await oracle2.submitValue( ethers.utils.formatBytes32String("2"),150,nonce);//clear inflationary rewards
+    await tellor.transfer(oracle.address,web3.utils.toWei("200"));//funding the oracle for inflationary rewards
     oracle = await ethers.getContractAt("contracts/Oracle.sol:Oracle",oracle.address, accounts[1]);
     await oracle2.addTip(ethers.utils.formatBytes32String("1"),web3.utils.toWei("10"))
     let initBal = await tellor.balanceOf(accounts[1].address)
